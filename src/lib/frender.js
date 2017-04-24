5e988b781011a4d0517e85519fe0c8babc4952e8
mc-tangb <511598230@qq.com>
2017-04-24 23:08:48 +0800

    折线连接

--- src/lib/frender.js
+++ src/lib/frender.js
@@ -13861,10 +13861,11 @@ define('frender/custom/TaskBlock',['require','../graphic/shape/Polygon','../grap
 			this._initElement();
 		},
 		drawLine: function (start, end) {
+            var that = this;
 			var group = new Group();
 			var pline = new Polyline({
 				shape: {
-					points: [[start.shape.cx + start.position[0], start.shape.cy + start.position[1]], [end.shape.cx + end.position[0], end.shape.cy + end.position[1]]],
+					points: that._handlerPolyline(start, end)//[[start.shape.cx + start.position[0], start.shape.cy + start.position[1]], [end.shape.cx + end.position[0], end.shape.cy + end.position[1]]],
 				}
 			})
 			var len = pline.shape.points.length;
@@ -13878,23 +13879,60 @@ define('frender/custom/TaskBlock',['require','../graphic/shape/Polygon','../grap
 			group.add(pline);
 			group.add(arrow);
 			start.follow.on('drag', function(){
-				var x1 = start.position[0] + start.shape.cx;
-				var y1 = start.position[1] + start.shape.cy;
-				var points = pline.shape.points;
-				points[0] = [x1, y1];
+				//var x1 = start.position[0] + start.shape.cx;
+				//var y1 = start.position[1] + start.shape.cy;
+				var points = that._handlerPolyline(start, end);//pline.shape.points;
+				//points[0] = [x1, y1];
 				pline.setShape('points', points);
 				arrow.setShape('points', points.slice(-2));
 			})
 			end.follow.on('drag', function(){
-				var x1 = end.position[0] + end.shape.cx;
-				var y1 = end.position[1] + end.shape.cy;
-				var points = pline.shape.points;
-				points[1] = [x1, y1];
+				//var x1 = end.position[0] + end.shape.cx;
+				//var y1 = end.position[1] + end.shape.cy;
+				var points = that._handlerPolyline(start, end);//pline.shape.points;
+				//points[1] = [x1, y1];
 				pline.setShape('points', points);
 				arrow.setShape('points', points.slice(-2));
 			})
 			this._zr.add(group);
 		},
+        _handlerPolyline: function (start, end) {
+            var startDir = start.dir,
+                endDir = end.dir,
+                startPoint = [start.position[0] + start.shape.cx, start.position[1] + start.shape.cy],
+                endPoint = [end.position[0] + end.shape.cx, end.position[1] + end.shape.cy],
+                dirStartPoint = directPoint(startDir, startPoint),
+                dirEndPoint = directPoint(endDir, endPoint),
+                middlePoint = [(dirStartPoint[0]+dirEndPoint[0])/2, (dirStartPoint[1]+dirEndPoint[1])/2],
+                middleBefore,
+                middleEnd;
+            if(dirStartPoint[1] > middlePoint[1] && dirEndPoint[1] < middlePoint[1]){
+                middleBefore = [middlePoint[0], dirStartPoint[1]],
+                
+                middleEnd = [middlePoint[0], dirEndPoint[1]];
+            }else {
+                middleBefore = [dirStartPoint[0], middlePoint[1]],
+                
+                middleEnd = [dirEndPoint[0], middlePoint[1]];
+            }
+            return [startPoint, dirStartPoint, middleBefore, middleEnd, dirEndPoint, endPoint];
+            function directPoint(dir, point){
+                var arr, len=25;
+                if(dir == 'top') {
+                    arr = [point[0], point[1] - len]; 
+                } else if (dir == 'right') {
+                    arr = [point[0] + len, point[1]];
+                } else if (dir == 'bottom') {
+                    arr = [point[0], point[1] + len];
+                } else if (dir == 'left') {
+                    arr = [point[0] - len, point[1]]
+                }
+
+                return arr;
+            }
+
+
+        },
 		_initElement: function () {
 			var that = this;
 			this._zr.on("mouseup", function (e) {
